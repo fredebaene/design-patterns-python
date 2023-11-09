50680887f50f8c152e50fe8dc065ec76e1afe636
Frederick De Baene <frederick.debaene@hotmail.com>
2023-11-09 23:31:23 +0100

    Add additional patterns

--- 06_command_pattern/garage.py
+++ 06_command_pattern/garage.py
@@ -2,14 +2,14 @@ class Garage:
     def __init__(self, id: str):
         self.id = id
 
-    def open_garage_door(self):
+    def open_garage_door(self) -> None:
         print(f"GARAGE : {self.id} | Door opening...")
 
-    def close_garage_door(self):
+    def close_garage_door(self) -> None:
         print(f"GARAGE : {self.id} | Door closing...")
 
-    def switch_on_garage_light(self):
+    def switch_on_garage_light(self) -> None:
         print(f"GARAGE : {self.id} | Switch on light...")
 
-    def switch_off_garage_light(self):
+    def switch_off_garage_light(self) -> None:
         print(f"GARAGE : {self.id} | Switch off light...")
